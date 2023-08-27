a724fde2d46307acb72d7e80dfedb6e3ea1383ba
adelson70 <129080230+adelson70@users.noreply.github.com>
2023-08-27 17:31:27 -0300

    Update conta_bancaria_v1.1.py

--- conta_bancaria_v1.1.py
+++ conta_bancaria_v1.1.py
@@ -1,153 +1,160 @@
-from time import sleep
-from os import system
+import textwrap
 
-def limpar_tela(): # função para limpar tela usando a biblioteca os
-    system("cls")
 
-def esperar(tempo=3): # função para criar uma espera para avançar para proxima linha de codigo
-    sleep(tempo) # usando a função time da biblioteca time
+def menu():
+    menu = """\n
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
 
-# função criada para verificar se um numero é inteiro e positivo
-numero_inteiro_positivo = True
-def verifica_numero_inteiro_positivo(numero):
-    global numero_inteiro_positivo # ira atribuir valores a variavel que esta fora do escopo da função
 
-    numero_inteiro_positivo = True
-    try:# tentara converter a string em int se der certo ira fazer uma verificação condicional
-        numero = int(numero)
-        if numero <= 0:
-            numero_inteiro_positivo = False
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:\tR$ {valor:.2f}\n"
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
 
-    except ValueError:# se não conseguir converter a string em int, significa que tem letras na string
-        numero_inteiro_positivo = False
+    return saldo, extrato
 
-# função criada para verificar se a string é um número
-e_numero = True
-def verifica_se_e_numero(numero):
-    global e_numero # ira atribuir valores a variavel que esta fora do escopo da função
-    try:# tentara converter a string em float se der certo ira fazer uma verificação condicional
-        numero = float(numero)
-        e_numero = True
 
-    except ValueError:# se não conseguir converter a string em float, significa que tem letras na string
-        e_numero = False
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
 
-menu = """
+    if excedeu_saldo:
+        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    elif excedeu_limite:
+        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
 
-=> """
+    elif excedeu_saques:
+        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n=== Saque realizado com sucesso! ===")
 
-# loop infinito onde ocorrera todo codigo do programa
-while True:
-    limpar_tela()
-    print(f"Saldo Atual R$ {saldo:.2f}") # imprime uma mensagem do saldo atual
-    opcao = input(menu) # imprime o menu podendo entrar com dados do teclado
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
 
-    # bloco de codigo que fara o deposito na conta bancaria
-    if opcao == "d":
-        limpar_tela()
-        valor_deposito = input("Digite um valor de deposito que seja um numero inteiro: ")
-        verifica_numero_inteiro_positivo(valor_deposito)
+    return saldo, extrato
 
-        while numero_inteiro_positivo == False: # enquando o usuario não informar um numero valido
-            limpar_tela()
-            print("Digite um número inteiro positivo!")
-            valor_deposito = input("Digite um valor de deposito que seja um numero inteiro: ")
-            verifica_numero_inteiro_positivo(valor_deposito)
-        
-        valor_deposito = int(valor_deposito)
-        
-        limpar_tela()
-        print(f"Você Depositou R$ {valor_deposito:.2f}")
-        esperar()
-        extrato += f"Deposito: R$ {valor_deposito:.2f}-"
-        saldo += valor_deposito
-        continue
 
-    # bloco de codigo que fara o saque da conta bancaria
-    elif opcao == "s":
-        limpar_tela()
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+    print("==========================================")
 
-        if saldo == 0:
-            print(f"Seu saldo é de R$ {saldo:.2f}")
-            esperar()
-            continue
 
-        if numero_saques == LIMITE_SAQUES:
-            print("Você atingiu seu limite de saque diario!")
-            esperar()
-            limpar_tela()
-            continue
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
 
-        valor_saque = input("Digite um valor a ser sacado: ")
-        verifica_se_e_numero(valor_saque)
-        
-        while e_numero == False: # enquando o usuario não informar um numero
-            limpar_tela()
-            print("Digite apenas números!")
-            valor_saque = input("Digite um valor a ser sacado: ")
-            verifica_se_e_numero(valor_saque)
-            limpar_tela()
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
 
-        valor_saque = float(valor_saque)
-        if valor_saque > 500:
-            print(f"Seu limite de saque por operação é de R$ {limite}")
-            esperar()
-            limpar_tela()
-            continue
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
 
-        if valor_saque > saldo:
-            print(f"Seu saldo é de R$ {saldo:.2f}")
-            print(f"Valor de saque solicitado foi maior que seu saldo!")
-            esperar()
-            continue
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
+
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            valor = float(input("Informe o valor do depósito: "))
+
+            saldo, extrato = depositar(saldo, valor, extrato)
+
+        elif opcao == "s":
+            valor = float(input("Informe o valor do saque: "))
+
+            saldo, extrato = sacar(
+                saldo=saldo,
+                valor=valor,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES,
+            )
+
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == "nu":
+            criar_usuario(usuarios)
+
+        elif opcao == "nc":
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+
+        elif opcao == "lc":
+            listar_contas(contas)
+
+        elif opcao == "q":
+            break
 
         else:
-            limpar_tela()
-            numero_saques += 1
-            print(f"Você sacou R$ {valor_saque:.2f}")
-            esperar()
-            extrato += f"Saque: R$ {valor_saque:.2f}-"
-            saldo -= valor_saque     
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
 
-    # bloco de codigo que ira imprimir o extrato da conta bancaria
-    elif opcao == "e":
-        limpar_tela()
 
-        # cria um arquivo de texto para ser o extrato bancario
-        extrato_bancario = open("extrato_bancario.txt","w")
-
-        for i in extrato:
-            if i == "-":
-                extrato_bancario.write("\n")
-            else:
-                extrato_bancario.write(i)
-
-        extrato_bancario.write(f"Saldo Atual: R$ {saldo:.2f}")
-        extrato_bancario.close()
-        print("Extrato Bancario Impresso com Sucesso!")
-        esperar(5)
-
-    # bloco de codigo que ira encerrar o loop
-    elif opcao == "q":
-        limpar_tela()
-        print("Encerrando")
-        esperar()
-        break
-    
-    # bloco de codigo que ira informar se o usuario entrou com algum comando invalido ao menu
-    else:
-        limpar_tela()
-        print("Comando Invalido!")
-        esperar()
-        continue
+main()
